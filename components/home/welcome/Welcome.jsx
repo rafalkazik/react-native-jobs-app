804e4a0ac744b4f8a0e698cf908f5054a5758c6e
Rafał Kazik <kazik.raf@gmail.com>
2023-11-23 19:33:30 +0100

    top buttons, fixes, changeS

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './welcome.style'
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
+import { useRouter } from 'expo-router';
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Rafał</Text>
+        <Text style={styles.welcomeMessage}>Find your dream job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
--- constants/images.js
+++ constants/images.js
@@ -1,4 +1,4 @@
-import profile from "../assets/images/kemal.jpg";
+import profile from '../assets/images/avatar.jpeg';
 
 export default {
   profile,
