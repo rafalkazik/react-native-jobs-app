fd30841aa6776133a79735cd342f26031ab8eaff
Rafał Kazik <kazik.raf@gmail.com>
2023-11-25 20:59:34 +0100

    job details

---
+++ app/job-details/[id].js
@@ -0,0 +1,114 @@
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native';
+import { Stack, useRouter, useGlobalSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+} from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from '../../hook/useFetch';
+import * as Sharing from 'expo-sharing';
+import * as FileSystem from 'expo-file-system';
+
+const tabs = ['About', 'Qualifications', 'Responsibilities'];
+
+const JobDetails = () => {
+  const params = useGlobalSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch('job-details', {
+    job_id: params.id,
+  });
+
+  const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const onRefresh = () => {};
+
+  const handleShare = async () => {
+    try {
+      if (await Sharing.isAvailableAsync()) {
+        const content = 'Treść do udostępnienia';
+        const filePath = `${FileSystem.cacheDirectory}share.txt`;
+
+        await FileSystem.writeAsStringAsync(filePath, content);
+
+        await Sharing.shareAsync(filePath);
+      } else {
+        console.log('Udostępnianie niedostępne na tym urządzeniu');
+      }
+    } catch (error) {
+      console.error('Błąd podczas udostępniania:', error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimensions='60%'
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.share}
+              dimensions='60%'
+              handlePress={handleShare}
+            />
+          ),
+          headerTitle: '',
+        }}
+      ></Stack.Screen>
+
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size='large' color={COLORS.primary} />
+          ) : error ? (
+            <Text>{'Something went wrong :/'}</Text>
+          ) : data.length === 0 ? (
+            <Text>{'No data'}</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            </View>
+          )}
+        </ScrollView>
+      </>
+    </SafeAreaView>
+  );
+};
+
+export default JobDetails;
--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import styles from './company.style';
+import { icons } from '../../../constants';
 
-import styles from './company.style'
-
-const Company = () => {
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri:
+              companyLogo === null
+                ? 'https://i.fbcd.co/products/resized/resized-1500-1000/35fa221b9125551fc13446133b8d980337655e15f27b10dfb205db407ab13975.webp'
+                : companyLogo,
+          }}
+          resizeMode='contain'
+          style={styles.logoImage}
+        />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode='contain'
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
